b3c2a056e40b5454ebf83215e9a62b1835bbef38
Mikhail <1pestilent@gmail.com>
2024-04-01 19:38:01 +0500

    Создал модель бд

--- clothesshop/clothesshop/settings.py
+++ clothesshop/clothesshop/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'clothesshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoweb',
+        'USER': 'postgres',
+        'PASSWORD': '123qwe',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
