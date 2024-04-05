61875d7e5e1fb620240d9edb75a03280522d7415
Mikhail <1pestilent@gmail.com>
2024-04-05 18:33:34 +0500

    Добавил шаблоны страниц, привязал контроллеры и вьюшки

--- clothesshop/products/admin.py
+++ clothesshop/products/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from products.models import ProductCategory,Product
+from users.models import User
 
 admin.site.register(Product)
 admin.site.register(ProductCategory)
+admin.site.register(User)
