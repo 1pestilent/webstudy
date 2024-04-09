38acef8f0b517f7098084e6987e5b77db2f462fa
Mikhail <1pestilent@gmail.com>
2024-04-09 20:50:36 +0500

    Добавлена корзина

--- clothesshop/users/views.py
+++ clothesshop/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
 
+from products.models import Basket
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 # Create your views here.
@@ -40,7 +41,12 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'form': form}
+
+    baskets = Basket.objects.filter(user=request.user)
+    context = {'title': 'Store - профиль',
+                'form': form,
+                'baskets': Basket.objects.filter(user=request.user), 
+            }
     return render(request, 'users/profile.html', context)
 
 def logout(request):
