655f40f794f9f59ca51a60e99fb2d59b87842ad1
Mikhail <1pestilent@gmail.com>
2024-04-10 11:07:46 +0500

    Добавлена фильтрация по категориям и пагинация

--- clothesshop/products/views.py
+++ clothesshop/products/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, HttpResponseRedirect
 from products.models import ProductCategory, Product, Basket
-from django.contrib.auth.decorators import login_required  
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def main(request):
     return render(request,"products/index.html")
 
-def products(request):
+def products(request, category_id=None, page_number=1):
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+    per_page = 3
+
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
+
     context = {
         'title': 'Store - Каталог',
-        'products': Product.objects.all(),
-        'categories':ProductCategory.objects.all()
+        'categories':ProductCategory.objects.all(),
+        'products': products_paginator,
     }
     return render(request,"products/products.html",context)
 
@@ -29,6 +36,7 @@ def basket_add(request, product_id):
     
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
+@login_required
 def basket_remove(request, basket_id):
     basket = Basket.objects.get(id=basket_id)
     basket.delete()
